fddeee50cda12499175247e0af2376814fe900a2
Jacob Elliott <jacob@jseis.me>
2021-09-13 09:33:42 -0400

    Update pin.py
    Why didn't I use zfill?

--- Task 4/pin.py
+++ Task 4/pin.py
@@ -1,21 +1,13 @@
 import hashlib
 
-x = [str(i) for i in range(10)]
-
-
 #Checks all possible 6-digit pins, including ones that start with 0
-for a in x:
-    for b in x:
-        for c in x:
-            for d in x:
-                for e in x:
-                    for f in x:
+for x in range(999999):
 
-                        X = a+b+c+d+e+f
+    X = str(x).zfill(6)
 
-                        H = hashlib.sha256(X.encode("utf-8")).hexdigest()
+    H = hashlib.sha256(X.encode("utf-8")).hexdigest()
 
-                        #Paste the bytes from the checkpin column in clientDB here
-                        if H == "7f72932585059397e5dfaca5cfa77515025e0c31e6e20f70ce814e0b3690f5ee":
-                            print(X,H)
-                            exit()
+    #Paste the bytes from the checkpin column in clientDB here
+    if H == "7f72932585059397e5dfaca5cfa77515025e0c31e6e20f70ce814e0b3690f5ee":
+        print(X,H)
+        exit()
